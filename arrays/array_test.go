8166b2102c4cea1b6838582ec9b36b269a1e523e
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-23 22:28:21 +0300

    Add more array tests

--- arrays/array_test.go
+++ arrays/array_test.go
@@ -26,3 +26,25 @@ func TestSumAll(t *testing.T) {
 	}
 
 }
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6})
+		want := []int{5, 11}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3})
+		want := []int{0, 5}
+
+		checkSums(t, got, want)
+	})
+}
