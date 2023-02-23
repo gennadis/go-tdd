786ea4dbbbdabbe56a1c588951e6b7eff5248bd7
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-23 18:54:10 +0300

    Say hello in multiple languages

--- hello/hello.go
+++ hello/hello.go
@@ -1,16 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+const spanish = "Spanish"
+const french = "French"
+const turkish = "Turkish"
 const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
+const frenchHelloPrefix = "Bonjour"
+const turkishhHelloPrefix = "Merhaba"
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return fmt.Sprintf("%s, %s!", englishHelloPrefix, name)
+
+	prefix := greetingPrefix(language)
+
+	return fmt.Sprintf("%s, %s!", prefix, name)
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case turkish:
+		prefix = turkishhHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("User"))
+	fmt.Println(Hello("User", "S"))
 }
