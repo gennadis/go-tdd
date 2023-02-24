528ce931a6fa67b46c677f6aa14eae50a98f4b0f
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-24 18:33:36 +0300

    Test dependency injections

---
+++ dep-injection/print_test.go
@@ -0,0 +1,18 @@
+package print
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "User")
+
+	got := buffer.String()
+	want := "Hello, User"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
