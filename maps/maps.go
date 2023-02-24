d324b6452e1d309121f5d7aaac0b480e1adee1c5
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-24 15:38:13 +0300

    Test Dictionary CRID API

---
+++ maps/maps.go
@@ -0,0 +1,57 @@
+package maps
+
+const (
+	ErrNotFound          = DictionaryError("could not find the word you were looking for")
+	ErrWordExists        = DictionaryError("word already exists")
+	ErrWordDoesNotExists = DictionaryError("cannot update because word does not exist")
+)
+
+type DictionaryError string
+
+func (e DictionaryError) Error() string {
+	return string(e)
+
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = definition
+	case ErrNotFound:
+		return ErrWordDoesNotExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
