2befcf0b34196cf397b9b263c6cd98238f596e6f
Gennady Badinov <gennady.badinov@gmail.com>
2023-02-26 13:59:43 +0300

    Add slow benchmark tests

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -3,10 +3,16 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
 	return url != "waat://furhurterwe.geds"
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
 }
 
 func TestCheckWebsites(t *testing.T) {
@@ -25,6 +31,19 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("expected %v got %v", want, got)
+	}
+
+}
+
+func BenchmarkCheckWebsite(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 1; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 
 }
